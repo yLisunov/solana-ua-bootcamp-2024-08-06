76d059a7fb4b837f9c4e2b4de5dfbb08ebdf3f85
yLisunov <lisykgerard45@gmail.com>
2024-08-12 12:00:45 +0300

    Add files via upload

---
+++ practice-1/check-balance.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import {
+  Connection,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  clusterApiUrl,
+} from "@solana/web3.js";
+
+const connection = new Connection(clusterApiUrl("devnet"));
+console.log(`⚡️ Connected to devnet`);
+
+const publicKey = new PublicKey("212oDcfdFkry8nFK3LiMdqTBdYMHeFb2bbjBk3NsJ7As");
+const balanceInLamports = await connection.getBalance(publicKey);
+
+const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
+
+console.log(
+  `💰 The balance for the wallet at address ${publicKey} is: ${balanceInSOL}`
+);
