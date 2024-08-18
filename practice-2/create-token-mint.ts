a0f659811701a5ceb5627f763d4a0faa9dfd387e
Хрисина Лісунова <anfield@MacBook-Pro-Hrisina.local>
2024-08-18 14:55:51 +0300

    second and third part

---
+++ practice-2/create-token-mint.ts
@@ -0,0 +1,36 @@
+import "dotenv/config";
+import {
+  getExplorerLink,
+} from "@solana-developers/helpers";
+import {
+  Keypair,
+  clusterApiUrl,
+  Connection,
+} from "@solana/web3.js";
+
+import { createMint } from "@solana/spl-token";
+
+let privateKey = process.env["SECRET_KEY"];
+if (privateKey === undefined) {
+  console.log("Add SECRET_KEY to .env!");
+  process.exit(1);
+}
+const asArray = Uint8Array.from(JSON.parse(privateKey));
+const sender = Keypair.fromSecretKey(asArray);
+
+const connection = new Connection(clusterApiUrl("devnet"));
+
+console.log(`🔑 Our public key is: ${sender.publicKey.toBase58()}`);
+
+const tokenMint = await createMint(
+    connection,
+    sender,
+    sender.publicKey,
+    null,
+    2
+  );
+  
+  const link = getExplorerLink("address", tokenMint.toString(), "devnet");
+  
+  console.log(`✅ Token Mint: ${link}`);
+  
--- practice-2/send-sol.ts
+++ practice-2/send-sol.ts
@@ -7,7 +7,8 @@ import {
   Transaction,
   clusterApiUrl,
   Connection,
-  sendAndConfirmTransaction
+  sendAndConfirmTransaction,
+  TransactionInstruction
 } from "@solana/web3.js";
 
 let privateKey = process.env["SECRET_KEY"];
@@ -21,3 +22,39 @@ const sender = Keypair.fromSecretKey(asArray);
 const connection = new Connection(clusterApiUrl("devnet"));
 
 console.log(`🔑 Our public key is: ${sender.publicKey.toBase58()}`);
+
+const recipient = new PublicKey("BaCMrNH5NrFydKtJW25kz5duYjHCmpM6MxcVJZuPX6W2");
+console.log(`💸 Attempting to send 0.01 SOL to ${recipient.toBase58()}...`);
+
+const transaction = new Transaction();
+
+const sendSolInstruction = SystemProgram.transfer({
+  fromPubkey: sender.publicKey,
+  toPubkey: recipient,
+  lamports: 1 * LAMPORTS_PER_SOL,
+});
+transaction.add(sendSolInstruction);
+
+const memoProgram = new PublicKey(
+    "MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr"
+  );
+  
+  const memoText = "Hello from Solana!";
+  
+  const addMemoInstruction = new TransactionInstruction({
+    keys: [{ pubkey: sender.publicKey, isSigner: true, isWritable: true }],
+    data: Buffer.from(memoText, "utf-8"),
+    programId: memoProgram,
+  });
+  
+  transaction.add(addMemoInstruction);
+  
+  console.log(`📝 memo is: ${memoText}`);
+  
+
+const signature = await sendAndConfirmTransaction(connection, transaction, [
+  sender,
+]);
+
+console.log(`✅ Transaction confirmed, signature: ${signature}!`);
+
