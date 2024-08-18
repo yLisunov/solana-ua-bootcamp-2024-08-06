d9705d31eac48708904fc97098fa407dd346c695
Хрисина Лісунова <anfield@MacBook-Pro-Hrisina.local>
2024-08-18 15:29:23 +0300

    2.5 and 2.6 tasks

---
+++ practice-2/create-token-metadata.ts
@@ -0,0 +1,81 @@
+import "dotenv/config";
+import {
+  Connection, clusterApiUrl, Keypair, PublicKey, sendAndConfirmTransaction, Transaction,
+} from "@solana/web3.js";
+import { getExplorerLink } from "@solana-developers/helpers";
+// Yes, createCreate! We're making an instruction for createMetadataV3...
+import { createCreateMetadataAccountV3Instruction } from "@metaplex-foundation/mpl-token-metadata";
+
+let privateKey = process.env["SECRET_KEY"];
+if (privateKey === undefined) {
+  console.log("Add SECRET_KEY to .env!");
+  process.exit(1);
+}
+const asArray = Uint8Array.from(JSON.parse(privateKey));
+const user = Keypair.fromSecretKey(asArray);
+
+const connection = new Connection(clusterApiUrl("devnet"));
+
+const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
+    "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+  );
+  
+  const tokenMintAccount = new PublicKey(
+    "55mApPVmcKfYFWvCHbTSfdsH9NYffx8FsttdPA54X72s"
+  );
+  
+  const metadataData = {
+    name: "Liverpool",
+    symbol: "LFC",
+    // Arweave / IPFS / Pinata etc link using metaplex standard for off-chain data
+    uri: "https://arweave.net/1234",
+    sellerFeeBasisPoints: 0,
+    creators: null,
+    collection: null,
+    uses: null,
+  };
+
+  const [metadataPDA, _metadataBump] = PublicKey.findProgramAddressSync(
+    [
+      Buffer.from("metadata"),
+      TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+      tokenMintAccount.toBuffer(),
+    ],
+    TOKEN_METADATA_PROGRAM_ID
+  );
+
+  const transaction = new Transaction();
+
+const createMetadataAccountInstruction =
+  createCreateMetadataAccountV3Instruction(
+    {
+      metadata: metadataPDA,
+      mint: tokenMintAccount,
+      mintAuthority: user.publicKey,
+      payer: user.publicKey,
+      updateAuthority: user.publicKey,
+    },
+    {
+      createMetadataAccountArgsV3: {
+        collectionDetails: null,
+        data: metadataData,
+        isMutable: true,
+      },
+    }
+  );
+transaction.add(createMetadataAccountInstruction);
+
+await sendAndConfirmTransaction(
+    connection,
+    transaction,
+    [user]
+  );
+  
+  const tokenMintLink = getExplorerLink(
+    "address",
+    tokenMintAccount.toString(),
+    "devnet"
+  );
+  console.log(`✅ Look at the token mint again: ${tokenMintLink}!`);
+  
+  
---
+++ practice-2/mint-tokens.ts
@@ -0,0 +1,38 @@
+import "dotenv/config";
+import { Connection, Keypair, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { mintTo } from "@solana/spl-token";
+import { getExplorerLink } from "@solana-developers/helpers";
+
+let privateKey = process.env["SECRET_KEY"];
+if (privateKey === undefined) {
+  console.log("Add SECRET_KEY to .env!");
+  process.exit(1);
+}
+const asArray = Uint8Array.from(JSON.parse(privateKey));
+const sender = Keypair.fromSecretKey(asArray);
+
+const connection = new Connection(clusterApiUrl("devnet"));
+
+// Our token has two decimal places
+const MINOR_UNITS_PER_MAJOR_UNITS = Math.pow(10, 2);
+
+const tokenMintAccount = new PublicKey(
+  "55mApPVmcKfYFWvCHbTSfdsH9NYffx8FsttdPA54X72s"
+);
+
+const recipientAssociatedTokenAccount = new PublicKey(
+    "CvyE7ZfFtJkVGVq87LsgpZfbbFszZXogBBXG9LXX6Nho"
+  );
+  
+  const transactionSignature = await mintTo(
+    connection,
+    sender,
+    tokenMintAccount,
+    recipientAssociatedTokenAccount,
+    sender,
+    10 * MINOR_UNITS_PER_MAJOR_UNITS
+  );
+  
+  const link = getExplorerLink("transaction", transactionSignature, "devnet");
+  
+  console.log(`✅ Success! Mint Token Transaction: ${link}`);
